128c2e8173251a54c1f42f323b3465f6d7d21490
Vasif T <Vasif T@VASIF>
2022-10-19 20:37:33 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var btn = sender as Button;
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+                btn!.Background = new SolidColorBrush(Color.FromRgb((byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255)));
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                grid.Children.Remove(btn);
+                Title = btn!.Content.ToString();
+            }
+        }
+    }
+}
